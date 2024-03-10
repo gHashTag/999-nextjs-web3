3f61dea66a31698f111899deeaedf5024d2b4d3e
AndreyO <andrey@railsmuffin.com>
2024-03-10 13:13:12 +0300

    meets page

--- src/pages/create-meet/index.tsx
+++ src/pages/create-meet/index.tsx
@@ -2,6 +2,7 @@ import LandingCards from "@components/revamp/LandingCards/LandingCards";
 import SubCard from "@/components/revamp/LandingCards/SubCard";
 import Layout from "@/components/layout";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const CreateMeet = () => {
   const router = useRouter();
@@ -16,6 +17,57 @@ const CreateMeet = () => {
   const getTokenGated = async () => {
     router.push(`/create-meet/token-gated`);
   };
+
+  const managementToken = process.env.NEXT_PUBLIC_MANAGEMENT_TOKEN;
+
+  if (!managementToken) {
+    throw new Error("NEXT_PUBLIC_100MS_TOKEN_SECRET is not set");
+  }
+
+  const getSessions = async () => {
+    try {
+      const response = await fetch("https://api.100ms.live/v2/sessions", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${managementToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Ошибка при получении сессий");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const getRecordings = async () => {
+    try {
+      const response = await fetch("https://api.100ms.live/v2/recordings", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${managementToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Ошибка при получении сессий");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  useEffect(() => {
+    // getSessions();
+    getRecordings();
+  }, []);
 
   return (
     <Layout>
